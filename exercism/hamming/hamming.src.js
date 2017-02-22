902f7e0222d23f94e73e0f8fceb9770173b6a44c
Briana Brown <brownbb1990@gmail.com>
2017-02-21 17:53:10 -0800

    adding function to  hamming test

---
+++ exercism/hamming/hamming.src.js
@@ -0,0 +1,19 @@
+
+//A function that gives no difference in the string
+ var newHamming = "GAGCCTACTAACGGGAT";
+ var hamming = "CGAGCCTACTAACGGGA";
+
+ function Hamming = (){
+   hamming.compute = function( hamming, newHamming){
+     var count = 0;
+     if( hamming.length !== newHamming.length ) {   
+     }
+   }
+
+
+ }
+
+ for(i = 0, i < array.length; i++)
+ }
+
+ module.export = Hamming;
