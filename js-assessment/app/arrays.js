ed8d50e0d482ab48bc83fc48809a8dbeba4b2cea
benjaminpettus <benjaminpettus@gmail.com>
2017-02-25 15:22:36 -0800

    WIP js-assessment Arrays

--- js-assessment/app/arrays.js
+++ js-assessment/app/arrays.js
@@ -1,39 +1,68 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
-
+  indexOf: function( arr, item ) {
+    let location = arr.indexOf( item );
+    return location;
   },
 
   sum: function(arr) {
-
+    let total = arr.reduce( ( a, b ) => a + b );
+    return total;
   },
 
-  remove: function(arr, item) {
+  remove: function( arr, item ) {
+    const result = []
 
+    for ( let i = 0; i < arr.length; i++ ) {
+      if ( arr[i] !== item ){
+        result.push( arr[i] )
+      }
+    }
+    return result
   },
 
   removeWithoutCopy: function(arr, item) {
 
-  },
+    for ( let i = 0; i < arr.length; i++ ) {
+
+      if ( arr[ i ] === item ){
+        arr.splice( i, 1 )
+        i -= 1
+      }
+    }
+   return arr;
+ },
 
   append: function(arr, item) {
+
+    arr.push( item );
+    return arr
 
   },
 
   truncate: function(arr) {
-
+    arr.pop()
+    return arr
   },
 
   prepend: function(arr, item) {
 
+    arr.unshift( item )
+    return arr
   },
 
   curtail: function(arr) {
 
+    arr.shift()
+    return arr
+
   },
 
   concat: function(arr1, arr2) {
+
+    arr3 = arr1.concat( arr2 )
+    return arr3
 
   },
 
