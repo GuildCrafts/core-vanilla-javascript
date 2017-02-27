63366f81e152e091df0bbe3664200a8e59e6d6b2
yaseenagag <yaseen.uc@gmail.com>
2017-02-27 15:20:35 -0800

    modify wordy and pass all tests

--- exercism/wordy/wordy.js
+++ exercism/wordy/wordy.js
@@ -7,33 +7,42 @@ module.exports = {
         'minus': '-',
         'multiplied': '*',
         'divided': '/'
-      },
-      this.sequence = []
-
+      }
     }
 
-    answer() {
-      let splitString = this.input.replace('?',"")
+    separateString( data ) {
+      return data.replace('?',"")
       .replace(/by/g,'').split(' ').slice(2)
+    }
+
+    validProblem( data ) {
       let operandsKeys = Object.keys(this.operands)
       let match = false
       operandsKeys.forEach(operand => {
-        if (splitString.includes(operand)) {
+        if (data.includes(operand)) {
           match = true
         }
       })
       if (!match) {
         throw new Error()
       }
-      let mathOperators = splitString.map((x) => {
+    }
+
+    turnIntoEquation( data ){
+      return data.map((x) => {
         if (this.operands.hasOwnProperty(x)) {
           return this.operands[x]
         } else {
           return x
         }
       })
-      let result = mathOperators.join(' ')
-      return eval(result)
+    }
+
+    answer() {
+      let splitString = this.separateString(this.input)
+      this.validProblem(splitString)
+      let mathOperators = this.turnIntoEquation(splitString)
+      return eval(mathOperators.join(' '))
     }
   }
 }
