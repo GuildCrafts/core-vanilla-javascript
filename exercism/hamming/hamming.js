03f6eaf0ae83d77925a3b8f67eb46a7065624781
thelady <dihaynes77@gmail.com>
2017-02-27 17:58:01 -0800

    created and ran hamming.js and test

---
+++ exercism/hamming/hamming.js
@@ -0,0 +1,13 @@
+var Hamming = function() {}
+
+Hamming.prototype.compute = function (aString, bString) {
+  console.log('we are running the compute function')
+  if(aString == bString) {
+    return 0
+  }
+
+
+
+}
+
+module.exports = Hamming
