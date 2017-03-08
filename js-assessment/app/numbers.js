59a6c4a4cdadfcaa63abd516f090cde949af5b3f
je-lopez <je.lopez@me.com>
2017-03-08 08:17:37 -0800

    updates results to pass tests in AboutObjects.js

--- js-assessment/app/numbers.js
+++ js-assessment/app/numbers.js
@@ -2,8 +2,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-  	var x = (00000000 + num.toString(2)).substr(-8)
-  	return parseInt(x[x.length - bit])
+  	return parseInt((00000000 + num.toString(2)).substr(-8)[8 - bit])
   },
 
   base10: function(str) {
