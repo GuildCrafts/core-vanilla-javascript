816b736e74217ceafbbd92b720b4885db34a2eed
Monica Williams <monicaestellaw@gmail.com>
2017-04-04 16:16:53 -0700

    some tests passing, need to work on handling uppercase and symbols

--- exercism/pangram/pangram.spec.js
+++ exercism/pangram/pangram.spec.js
@@ -2,22 +2,22 @@ var Pangram = require('./pangram');
 
 describe('Pangram()', function()  {
 
-  it('empty sentence', function() {
+  xit('empty sentence', function() {
     var pangram = new Pangram('');
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit('pangram with only lower case', function()  {
+  it('pangram with only lower case', function()  {
     var pangram = new Pangram("the quick brown fox jumps over the lazy dog");
     expect(pangram.isPangram()).toBe(true);
   });
 
-  xit("missing character 'x'", function()  {
+  it("missing character 'x'", function()  {
     var pangram = new Pangram("a quick movement of the enemy will jeopardize five gunboats");
     expect(pangram.isPangram()).toBe(false);
   });
 
-  xit("another missing character 'x'", function() {
+  it("another missing character 'x'", function() {
     var pangram = new Pangram("the quick brown fish jumps over the lazy dog");
     expect(pangram.isPangram()).toBe(false);
   });
