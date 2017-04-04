48123bdcc20f7bf35046a8b58c06015a71697c98
sashajustice <sabrinnasreddin@gmail.com>
2017-04-03 18:08:07 -0700

    starts binary search, punit and frankie told us what it meant

---
+++ exercism/binary-search/binary-search.js
@@ -0,0 +1,35 @@
+function BinarySearch( arr ) {
+//an object taht has a state and the reason it can change is because it has some
+//data it's holding onto and some functions that can act on that data.
+//that means there's a state change
+  this.arr = arr
+}
+
+BinarySearch.prototype.indexOf = function( value ) {
+  //check if this.arr is sorted
+  //
+
+  // on the prototoype of bin search we're gonna have a method called index of
+
+}
+
+  java
+
+    var checkIfArrayIsSorted = function(arr) {
+      for(var i = 0; i < arr.length; i ++) {
+        if (arr[i] < arr[i+1]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+  }
+
+c++
+
+
+    for(var i = 0; i < thing.length; i++) {
+            if (thing[i] > thing[i+1]) {
+                console.log('false');
+            }
+        }
