001b3d8666143e56a1053d028315e97dd57484f9
Monica Williams <monicaestellaw@gmail.com>
2017-04-05 15:55:40 -0700

    changing branches

--- exercism/anagram/anagram.js
+++ exercism/anagram/anagram.js
@@ -1,19 +1,22 @@
 function Anagram( str ) {
-  this.str = str,
+  this.str = str.toLowerCase(),
   this.letterHash = {}
 }
 
-Anagram.prototype.matches = function( array ) {
+Anagram.prototype.matches = function( ...input ) {
   // loop through all chars in str and mark # of time each letter apears
   let foundAnagrams = []
   this.hashLetterCount()
-  console.log(this.letterHash);
-  console.log('the array we passed in:', array);
+
   // loop through all of the words we are checcking to see if they
   // are anagrams of this.str
-  for( let word in array ) {
-    if( this.isAnagram( word ) ){
-      foundAnagrams.push(word)
+  if ( input[0] instanceof Array ) {
+    
+    for( let word of input ) {
+      console.log('word', word)
+      if( this.isAnagram( word )) {
+        foundAnagrams.push(word)
+      }
     }
   }
 
@@ -31,8 +34,34 @@ Anagram.prototype.hashLetterCount = function() {
 }
 
 Anagram.prototype.isAnagram = function( word ) {
-  
+  word = word.toLowerCase()
+  if ( word === this.str ) {
+    return false
+  }
+  if ( word.length !== this.str.length ) {
+    return false
+  }
+  for( let letter of word ) {
+    console.log('word', word)
+    if( this.letterHash[letter] === undefined ) {
+      return false
+    } else {
+      this.letterHash[letter]--
+    }
+  }
+  for( let letter in this.letterHash ) {
+    if ( this.letterHash[letter] > 0 ) {
+      return false
+    }
+  }
+  return true
+  //loop through letterHash.
+  //if there are any values that are still greater than 0, return false
+  //word = the word at any index of the array
+  //check each of those words against this.str
 }
 
 let myAnagram = new Anagram( 'hello' )
-console.log( myAnagram.matches( ['elloh'] ) );
+myAnagram.matches( ['olleh', 'goodbye'])
+
+module.exports = Anagram
