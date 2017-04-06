cfcb6eb2521ef9ab5de189e5fe57695369364678
sashajustice <sabrinnasreddin@gmail.com>
2017-04-06 15:00:29 -0700

    start code for etl

---
+++ exercism/etl/etl.js
@@ -0,0 +1,18 @@
+function Etl () {
+  this.{} = {};
+  this.transformLetters {1 point: "A", "E", "I", "O", "U", "L", "N", "R", "S", "T",
+2 points: "D", "G",
+3 points: "B", "C", "M", "P",
+4 points: "F", "H", "V", "W", "Y",
+5 points: "K",
+8 points: "J", "X",
+ 10 points: "Q", "Z",}
+  }
+}
+
+Etl.prototype.transform = function () {
+  if( )
+
+}
+
+module.exports = Etl
