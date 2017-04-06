30e3ddedfdce002692432426503216d2d7705da0
Monica Williams <monicaestellaw@gmail.com>
2017-04-06 16:16:48 -0700

    things are working but the output is not alphabetized, the oldData is hardcoded

--- exercism/etl/etl.js
+++ exercism/etl/etl.js
@@ -1,18 +1,39 @@
-function Etl () {
-  this.{} = {};
-  this.transformLetters {1 point: "A", "E", "I", "O", "U", "L", "N", "R", "S", "T",
-2 points: "D", "G",
-3 points: "B", "C", "M", "P",
-4 points: "F", "H", "V", "W", "Y",
-5 points: "K",
-8 points: "J", "X",
- 10 points: "Q", "Z",}
+function ETL() {
+  this.newData = {};
+  this.transformLetters  = {
+    1: [ 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' ],
+    2: [ 'D', 'G' ],
+    3: [ 'B', 'C', 'M', 'P' ],
+    4: [ 'F', 'H', 'V', 'W', 'Y' ],
+    5: [ 'K' ],
+    8: [ 'J', 'X' ],
+    10: [ 'Q', 'Z' ]
   }
 }
 
-Etl.prototype.transform = function () {
-  if( )
+ETL.prototype.transform = function( oldData ) {
+  for ( prop in this.transformLetters ) {
+    var individualArray = this.transformLetters[prop]
+
+    while ( individualArray.length > 0 ) {
+      var letter = individualArray.pop().toLowerCase()
+      this.newData[letter] = prop
+    }
+  }
+
+  return this.newData
 
 }
 
-module.exports = Etl
+module.exports = ETL
+
+//loop through Object and record key.
+//for each key, access array
+//for each array, pop off until the array is empty
+  //as we pop off, push value.lowerCase to this.newData as the key, and the key of the array as the value
+//return newData
+
+var scrabble = new ETL()
+console.log(scrabble.transform())
+
+//this.transformLetters[prop] gets us all of the arrays without their keys
